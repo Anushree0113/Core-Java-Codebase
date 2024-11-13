d2ec83b057f601fcab639b5d078c042f2b5545d3
anushree <anushreepaul0113@gmail.com>
2024-11-14 01:27:08 +0530

    Interview Preparation - DP Array Questions

---
+++ src/main/java/leetcode/Array/ContainsDuplicate.java
@@ -0,0 +1,24 @@
+package leetcode.Array;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        boolean result = containsDuplicate(nums);
+        System.out.println("result = " + result);
+    }
+
+    private static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int val : nums) {
+            if (seen.contains(val)) {
+                return true;  // Found a duplicate
+            }
+            seen.add(val);  // Add the current value to the set
+        }
+
+        return false;
+    }
+}
