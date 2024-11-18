69402c421fffde6c5638bd5ac9dab3f0a3b59e60
anushree <anushreepaul0113@gmail.com>
2024-11-18 16:49:23 +0530

    Interview Preparation -  Thread Basics

---
+++ src/main/java/Thread/ThreadCreate.java
@@ -0,0 +1,21 @@
+package Thread;
+
+class B implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+}
+
+public class ThreadCreate {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+
+        Thread thread = new Thread(new B());
+        thread.start();
+    }
+}
